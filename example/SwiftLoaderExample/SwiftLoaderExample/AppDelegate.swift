c4361b1406234a105b678f4a157d521c8c7fe37f
Mikael Bartlett <mikael@nixonnixon.se>
2018-12-17 23:13:47 +0100

    Converted to swift 4.2

--- example/SwiftLoaderExample/SwiftLoaderExample/AppDelegate.swift
+++ example/SwiftLoaderExample/SwiftLoaderExample/AppDelegate.swift
@@ -14,32 +14,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
         
         let vc = ViewController()
         self.window?.rootViewController = vc
         self.window?.makeKeyAndVisible()
         
-        self.window?.becomeKeyWindow()
+        self.window?.becomeKey()
         return true
     }
-
-    func applicationWillResignActive(application: UIApplication) {
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-    }
-
 
 }
 
